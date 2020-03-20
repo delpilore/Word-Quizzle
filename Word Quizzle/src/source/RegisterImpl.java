6df42b5054fcf53cd17fd28744e7a93499e36b39
delpilore <loredelprete@live.it>
2020-03-20 18:31:48 +0100

    Modificato costruttore e metodo registra_utente (funziona) :
    Attualmente non sono sicuro se ogni volta che un utente chiama
    registra_utente, il metodo writeValue riscrive tutta la Hashtable
    in un file JSON o "appende" semplicemente il nuovo valore inserito.
    Io vorrei succedesse quest'ultima cosa in modo da rendere più
    efficiente il tutto.
    
    Sono sicuro, invece, che il server istanzia una nuova hashtable solo se
    non esiste già un file JSON da dove ricostruire la stessa senza
    istanziarne una nuova.

--- Word Quizzle/src/source/RegisterImpl.java
+++ Word Quizzle/src/source/RegisterImpl.java
@@ -1,20 +1,54 @@
 package source;
 
+import java.io.File;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.server.RemoteServer;
 import java.util.Hashtable;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RegisterImpl extends RemoteServer implements RegisterInterface {
 
 	private static final long serialVersionUID = 1L;
 	Hashtable<String, String> registered_users;
+	ObjectMapper objectMapper = new ObjectMapper();
+	File json_file;
 	
 	public RegisterImpl() throws RemoteException {
-		registered_users = new Hashtable <String,String>();
+		json_file = new File("registered_users.json");
+		if(!json_file.isFile()) {
+			registered_users = new Hashtable <String, String>();
+		}
+		else {
+			System.out.println("Il file JSON esiste già, recupero i valori dal file");
+			try {
+				registered_users = objectMapper.readValue(json_file, new TypeReference<Hashtable<String, String>>(){});
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Gli utenti registrati sono: " + registered_users); 
+		}
+		
 	}
-	public int registra_utente (String nickUtente, String password) throws RemoteException {
+	
+	
+	public boolean registra_utente (String nickUtente, String password) throws RemoteException, UserAlreadyRegisteredException, NullPointerException {
+		
+		if (registered_users.containsKey(nickUtente))
+			throw new UserAlreadyRegisteredException();
+		
+		if (nickUtente==null || password==null)
+			throw new NullPointerException();
+		
 		registered_users.put(nickUtente, password);
 		System.out.println("Utente " + nickUtente + " registrato con successo!");
-		return 1;
+		try {
+			objectMapper.writeValue(json_file, registered_users);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
 	}
 }
