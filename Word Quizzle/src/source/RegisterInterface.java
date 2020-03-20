7ba9d451ad00af13519c833e59b67d6846765639
delpilore <loredelprete@live.it>
2020-03-20 18:26:20 +0100

    Aggiunta l'eccezione UserAlreadyRegisteredException

--- Word Quizzle/src/source/RegisterInterface.java
+++ Word Quizzle/src/source/RegisterInterface.java
@@ -4,5 +4,18 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RegisterInterface extends Remote {
-	int registra_utente (String nickUtente, String password) throws RemoteException;
+	
+	boolean registra_utente (String nickUtente, String password) throws RemoteException, UserAlreadyRegisteredException, NullPointerException;
+	
+	@SuppressWarnings("serial")
+	class UserAlreadyRegisteredException extends Exception {
+
+		public UserAlreadyRegisteredException() {
+            super();
+        }
+        
+        public UserAlreadyRegisteredException(String s) {
+            super(s);
+        }
+    }
 }
