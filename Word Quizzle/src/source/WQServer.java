9033c90a764aaf4776e62a8d5d52e5221b5804f7
delpilore <loredelprete@live.it>
2020-03-21 22:02:03 +0100

    Aggiunta operazione stopJSON per chiudere la scrittura del file JSON
    PROVVISORIO

--- Word Quizzle/src/source/WQServer.java
+++ Word Quizzle/src/source/WQServer.java
@@ -4,13 +4,16 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 public class WQServer {
 	
 	public static void main(String[] args) {
 		
+		Structures support = new Structures();
+		
 		try {
-			RegisterImpl register = new RegisterImpl();
+			RegisterImpl register = new RegisterImpl(support);
 			RegisterInterface stub = (RegisterInterface) UnicastRemoteObject.exportObject(register, 0);
 
 			LocateRegistry.createRegistry(15000);
@@ -19,6 +22,15 @@ public class WQServer {
 			r.rebind("REGISTER-SERVER", stub);
 
 			System.out.println("Server ready");
+			
+			/* Stop dell'input JSON */
+			Scanner input = new Scanner(System.in);
+			String command;
+			command = input.next();
+			if(command.equals("stop"))
+				register.stopJSON();
+			input.close();
+			
 		}
 		catch(RemoteException e){
 			e.printStackTrace();
