17c483ad0604e186b7ea81e89979314f79ccab33
delpilore <loredelprete@live.it>
2020-03-22 18:52:15 +0100

    Incapsulato tutto ciò che concerne il file JSON in questa classe
    Al momento della creazione dell'oggetto vengono fatti tutti i controlli
    del caso sul file JSON (presente o no, vuoto o no) per poi essere
    sfruttato per ricostruire la tabella interna WordQuizzleUsers
    La scrittura di esso da parte di altri oggetti avverrà semplicemente
    chiamando il metodo writeJson sull'oggetto Structures.

--- Word Quizzle/src/source/Structures.java
+++ Word Quizzle/src/source/Structures.java
@@ -1,25 +1,65 @@
 package source;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Hashtable;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Structures {
 
-	private Hashtable<String, User> registered_users;  // Mappa il nome utente di un utente registrato al suo oggetto User, contentente altre informazioni (per ora solo password)
+	private Hashtable<String, User> WordQuizzleUsers;  // Mappa il nome utente di un utente registrato al suo oggetto User, contentente altre informazioni (per ora solo password)
+	private File json_file;
+	
+	private ObjectMapper objectMapper = new ObjectMapper();
 	
 	public Structures() {
-		registered_users = new Hashtable<String,User>();
+		WordQuizzleUsers = new Hashtable<String,User>();
+		json_file = new File("WordQuizzleUsers.json");
+		
+		if(json_file.isFile()) {
+			System.out.println("Il file JSON esiste già, recupero tutti gli utenti dal file\n");
+
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(json_file));
+				
+				if (br.readLine() != null) {
+					WordQuizzleUsers = objectMapper.readValue(json_file, new TypeReference<Hashtable<String,User>>(){});
+					System.out.println("Gli utenti registrati sono: " + WordQuizzleUsers); // Stampo tutto il contenuto della Hashtable dei registrati per vedere se è consistente
+				}
+				else
+					System.out.println("Il file JSON esiste già ma è vuoto\n");
+				br.close();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else
+			System.out.println("Il file JSON non esiste, avvio pulito del server\n");
 	}
 	
-	public void reg_addUser(String _username, User _user) {
-		registered_users.put(_username, _user);
+	public void addUser(String _username, User _user) {
+		WordQuizzleUsers.put(_username, _user);
 	}
 	
-	public boolean reg_containsUser(String _username) {
-		return registered_users.containsKey(_username);
+	public boolean containsUser(String _username) {
+		return WordQuizzleUsers.containsKey(_username);
 	}
 	
-	public Hashtable<String, User> getRegistered(){
-		return registered_users;
+	public void writeJson() {
+		try {
+			objectMapper.writeValue(json_file, WordQuizzleUsers);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public User getUser(String _user) {
+		return WordQuizzleUsers.get(_user);
 	}
 	
 }
