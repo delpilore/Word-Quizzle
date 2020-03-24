46403fce9da95bc343dc7464636a87b8784d3809
delpilore <loredelprete@live.it>
2020-03-24 17:34:43 +0100

    Aggiunti StatusCodes
    - aggiunta amico verso un utente inesistente
    - aggiunta amico ad un utente con cui si è già amici

--- Word Quizzle/src/source/StatusCodes.java
+++ Word Quizzle/src/source/StatusCodes.java
@@ -3,7 +3,9 @@ package source;
 public enum StatusCodes {
 	USERNOTREGISTERED("Non sei registrato!\n"),
 	WRONGPASSWORD("La password inserita non è corretta!\n"),
-	OK("L'operazione è andata a buon fine!\n");
+	OK("L'operazione è andata a buon fine!\n"),
+	WRONGFRIENDREQUEST("L'utente specificato non è registrato a Word Quizzle!\n"),
+	ALREADYFRIENDS("Sei già amico con questo utente!\n");
 	
     public final String label;
     
