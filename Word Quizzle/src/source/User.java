369476f6770bc4d8f90e51dba0ea54c2edfa00ec
delpilore <loredelprete@live.it>
2020-03-24 12:56:55 +0100

    Aggiunto scheletro per implementare le operazioni sulle amicizie

--- Word Quizzle/src/source/User.java
+++ Word Quizzle/src/source/User.java
@@ -1,10 +1,13 @@
 package source;
 
+import java.util.ArrayList;
+
 public class User {
 	
 	private String username;
 	private String password;
 	private Boolean OnlineState=false;
+	private ArrayList<User> FriendList;
 
 	public User(String _username, String _password) {
 		setUsername(_username);
@@ -33,11 +36,22 @@ public class User {
 	}
 	
 	public void setOnlineState(Boolean _value) { 
-		OnlineState = _value;
+		this.OnlineState = _value;
 	}
 	
 	public boolean getOnlineState() {
 		return OnlineState;
 	}
 
+	public ArrayList<User> getFriendList() {
+		return FriendList;
+	}
+
+	public void setFriendList(ArrayList<User> _friendList) {
+		this.FriendList = _friendList;
+	}
+	
+	public void addFriend(User _user) {
+		FriendList.add(_user);
+	}
 }
