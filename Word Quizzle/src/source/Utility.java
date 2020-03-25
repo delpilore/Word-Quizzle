f732a40325bcaa66dc54cb14e2b98ac968e47931
delpilore <loredelprete@live.it>
2020-03-25 12:54:40 +0100

    Implementata funzione di utility che ordina la classifica degli amici
    A seconda del punteggio e restituisce un oggetto JSON relativo

--- Word Quizzle/src/source/Utility.java
+++ Word Quizzle/src/source/Utility.java
@@ -6,6 +6,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Utility {
 	public static void write(Socket receiver, Object request) {
@@ -26,5 +32,27 @@ public class Utility {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	public static JsonNode getRanking(Structures _support, String usr) {
+		ArrayList<User> Ranking = new ArrayList<User>();
+		ArrayList<String> friendList = _support.getUser(usr).getFriendList();
+		
+		Ranking.add(_support.getUser(usr));
+		
+		for (String friend : friendList) {
+			Ranking.add(_support.getUser(friend));
+		}
+		
+		Collections.sort(Ranking,Collections.reverseOrder(new ScoreComparator()));
+		
+		ObjectMapper rankingMapper = new ObjectMapper();
+		JsonNode rankingTable = rankingMapper.createObjectNode();
+		
+		for (User friend : Ranking) {
+			((ObjectNode) rankingTable).put(friend.getUsername(), friend.getScore());
+		}
+
+		return rankingTable;
 	}
 }
