7b671592656fb132ef005e16c72f891f89b2ffcc
delpilore <loredelprete@live.it>
2020-03-25 12:52:36 +0100

    Refusi

--- Word Quizzle/src/source/StatusCodes.java
+++ Word Quizzle/src/source/StatusCodes.java
@@ -6,7 +6,7 @@ public enum StatusCodes {
 	OK("L'operazione è andata a buon fine!\n"),
 	WRONGFRIENDREQUEST("L'utente specificato non è registrato a Word Quizzle!\n"),
 	ALREADYFRIENDS("Sei già amico con questo utente!\n");
-	
+
     public final String label;
     
     private StatusCodes(String label) {
