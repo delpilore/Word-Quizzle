d814892f6e31ee0a5a0d7b56e7216fe6c5f24b59
delpilore <loredelprete@live.it>
2020-03-26 11:24:18 +0100

    Commentato e controllato

--- Word Quizzle/src/source/RegisterImpl.java
+++ Word Quizzle/src/source/RegisterImpl.java
@@ -3,18 +3,35 @@ package source;
 import java.rmi.RemoteException;
 import java.rmi.server.RemoteServer;
 
+//AUTHOR: Lorenzo Del Prete, Corso B, 531417
+
+/*
+* REGISTERIMPL
+* 
+* Oggetto remoto che si occuperà di realizzare l'operazione di registrazione.
+*/
 
 public class RegisterImpl extends RemoteServer implements RegisterInterface {
 
-	private static final long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = -5222461577976164582L;
+	
+	// Da costruttore gli viene passato l'unico oggetto Structures del server, contenente, tra le altre,
+	// la HashTable che mappa gli utenti registrati al servizio.
 	private Structures WordQuizzleUsers;
 	
 	public RegisterImpl(Structures _support) throws RemoteException {
 		WordQuizzleUsers = _support;
 	}
 	
+	// registra_utente(String nickUtente, String password)
+	//
+	// Metodo chiamato in RMI da parte del client, che aggiunge un nuovo oggetto User (vedere "User") 
+	// alla Hashtable principale del server, quella che mappa gli utenti registrati al servizio
+	// e i loro dati. (vedere "Structures") 
+	// Realizza quindi una registrazione dell'utente.
 	public boolean registra_utente (String nickUtente, String password) throws RemoteException, UserAlreadyRegisteredException, NullPointerException, WeakPasswordException, UsernameTooShortException {
+		
+		// ECCEZIONI //
 		
 		if (nickUtente==null || password==null)
 			throw new NullPointerException();
@@ -27,7 +44,12 @@ public class RegisterImpl extends RemoteServer implements RegisterInterface {
 		
 		if (WordQuizzleUsers.containsUser(nickUtente))
 			throw new UserAlreadyRegisteredException();
+		
+		// FINE ECCEZIONI //
 
+		// Aggiungo una nuova coppia <nickUtente, new User> alla HashTable dei registrati.
+		// Successivamente, chiamando writeJson() (vedere "Structures") scrivo/aggiorno il file 
+		// json relativo allo stato attuale degli utenti registrati.
 		WordQuizzleUsers.addUser(nickUtente, new User(nickUtente, password));
 		WordQuizzleUsers.writeJson();
 		
--- Word Quizzle/src/source/RegisterInterface.java
+++ Word Quizzle/src/source/RegisterInterface.java
@@ -3,10 +3,31 @@ package source;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+//AUTHOR: Lorenzo Del Prete, Corso B, 531417
+
+/*
+* REGISTERINTERFACE
+* 
+* Interfaccia dell'oggetto remoto che si occuperà di realizzare l'operazione di registrazione.
+*/
+
 public interface RegisterInterface extends Remote {
 	
-	boolean registra_utente (String nickUtente, String password) throws RemoteException, UserAlreadyRegisteredException, NullPointerException, UsernameTooShortException, WeakPasswordException ;
+	// registra_utente(String nickUtente, String password)
+	//
+	// Metodo chiamato in RMI da parte del client, che aggiunge un nuovo oggetto User (vedere "User") 
+	// alla Hashtable principale del server, quella che mappa gli utenti registrati al servizio
+	// e i loro dati. (vedere "Structures") 
+	// Realizza quindi una registrazione dell'utente.
+	boolean registra_utente (String nickUtente, String password) throws RemoteException, UserAlreadyRegisteredException, 
+																		NullPointerException, UsernameTooShortException, 
+																		WeakPasswordException ;
 	
+	// UserAlreadyRegisteredException 
+	//
+	// Eccezione checked non presente in Java.
+	// Viene sollevata quando un client prova registrarsi al servizio utilizzando un username di un utente
+	// già registrato.
 	@SuppressWarnings("serial")
 	class UserAlreadyRegisteredException extends Exception {
 
@@ -19,6 +40,11 @@ public interface RegisterInterface extends Remote {
         }
     }
 	
+	// UsernameTooShortException
+	//
+	// Eccezione checked non presente in Java.
+	// Viene sollevata quando un client prova registrarsi al servizio utilizzando un username 
+	// più corto di 3 caratteri.
 	@SuppressWarnings("serial")
 	class UsernameTooShortException extends Exception {
 
@@ -31,6 +57,11 @@ public interface RegisterInterface extends Remote {
         }
     }
 	
+	// WeakPasswordException
+	//
+	// Eccezione checked non presente in Java.
+	// Viene sollevata quando un client prova registrarsi al servizio utilizzando un password 
+	// più corta di 4 caratteri
 	@SuppressWarnings("serial")
 	class WeakPasswordException extends Exception {
 
