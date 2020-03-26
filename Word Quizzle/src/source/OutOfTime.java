e883d4f6c39fd23cb19eb3d8331aa960b3542894
delpilore <loredelprete@live.it>
2020-03-26 19:40:09 +0100

    Timer per l'accettazione della richiesta di sfida
    Si tratta di un task (una routine) che viene eseguita in caso un utente
    non abbia accettato una richiesta di sfida entro un determinato limite
    di tempo

---
+++ Word Quizzle/src/source/OutOfTime.java
@@ -0,0 +1,36 @@
+package source;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.util.TimerTask;
+
+public class OutOfTime extends TimerTask {
+	
+	ClientChallengeListener Listener;
+	
+	public OutOfTime(ClientChallengeListener _listener) {
+		Listener = _listener;
+	}
+	
+    public void run() {
+    	// Questa routine avviene solo se l'utente risulta sempre "sfidato" ovvero
+    	// se sono passati 5 secondi e lui non ha ancora accettato (che avrebbe settato il flag a false)
+    	if(Listener.isChallenged()) {
+	        System.out.println( "Finito il tempo" );
+			Listener.setChallenged(false);
+	        
+	    	try {
+	        	InetAddress ip = InetAddress.getLocalHost(); 
+	        
+	        	String command = new String("t");
+	    		byte[] send = command.getBytes();
+				DatagramPacket DpSend = new DatagramPacket(send, send.length, ip, Listener.getServerUDPPort()); 
+				
+				Listener.getDatagramSocket().send(DpSend);
+	    	}
+	    	catch(Exception e) {
+	    		e.printStackTrace();
+	    	}
+    	}
+    }
+}
